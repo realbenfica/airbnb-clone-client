7a898fd80b69dc406aaf06aa80bf937d1d8910fa
realbenfica <alexgtr84@gmail.com>
2019-03-20 16:21:19 +0100

    Add naming conventions

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -15,7 +15,7 @@ const TopBar = (props) => {
     <AppBar position="absolute" style={{ zIndex: 10 }}>
       <Toolbar>
         <Typography variant="title" color="inherit" style={{ flex: 1 }}>
-          airbnb clone
+          Bed and Breakfast
         </Typography>
         {
           user &&
